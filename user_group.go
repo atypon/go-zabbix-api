2d37ae21fc4bda6a0959b6d2c342a32a553781ad
Malek Adawi <madawi@atypon.com>
2022-12-20 15:23:51 +0300

    IT-18227

--- user_group.go
+++ user_group.go
@@ -14,22 +14,40 @@ const (
 // UserGroup represent Zabbix user group object
 // https://www.zabbix.com/documentation/4.0/manual/api/reference/usergroup/object
 type UserGroup struct {
-	GroupID    string        `json:"usrgrpid,omitempty"`
-	Name       string        `json:"name"`
-	DebugMode  DebugModeType `json:"debug_mode,string,omitempty"`
-	GuiAccess  int           `json:"gui_access,string,omitempty"`
-	UserStatus StatusType    `json:"users_status,string,omitempty"`
+	GroupID             string        `json:"usrgrpid,omitempty"`
+	Name                string        `json:"name"`
+	DebugMode           DebugModeType `json:"debug_mode,string,omitempty"`
+	GuiAccess           int           `json:"gui_access,string,omitempty"`
+	UserStatus          StatusType    `json:"users_status,string,omitempty"`
+	UserIDs             []string      `json:"users,omitempty"`
+	HostGroupRights     []Permission  `json:"hostgroup_rights,omitempty"`
+	TemplateGroupRights []Permission  `json:"templategroup_rights,omitempty"`
 }
+
+type Permission struct {
+	ID         string          `json:"id"`
+	Permission PermissionLevel `json:"permission"`
+}
+
+type PermissionLevel int
+
+const (
+	AccessDenied PermissionLevel = iota
+	ReadOnly
+	ReadWrite
+)
 
 // UserGroups is an array of UserGroup
 type UserGroups []UserGroup
 
-// UserGroupsGet Wrapper for usergroup.get
-// https://www.zabbix.com/documentation/4.0/manual/api/reference/usergroup/get
-func (api *API) UserGroupsGet(params Params) (res UserGroups, err error) {
-	if _, present := params["output"]; !present {
-		params["output"] = "extend"
-	}
-	err = api.CallWithErrorParse("usergroup.get", params, &res)
-	return
+func (u *UserGroup) GetID() string {
+	return u.GroupID
+}
+
+func (u *UserGroup) SetID(ID string) {
+	u.GroupID = ID
+}
+
+func (u *UserGroup) GetAPIModule() string {
+	return "usergroup"
 }
--- user_group_test.go
+++ user_group_test.go
@@ -1,24 +1,11 @@
 package zabbix_test
 
 import (
-	"testing"
-
 	zapi "github.com/claranet/go-zabbix-api"
+	"testing"
 )
 
-func TestUserGroupsGet(t *testing.T) {
-	api := testGetAPI(t)
-
-	params := zapi.Params{
-		"filter": map[string]interface{}{
-			"name": "Zabbix administrators",
-		},
-	}
-	userGroups, err := api.UserGroupsGet(params)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(userGroups) != 1 {
-		t.Errorf("Bad user groups: %#v", userGroups)
-	}
+func TestUserGroup(t *testing.T) {
+	group := &zapi.UserGroup{Name: "TestUserGroup"}
+	testCRUDAPIObjectOperations(t, group)
 }
