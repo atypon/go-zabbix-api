dc18d5987c39f55890e66473602c18885db98348
Malek Adawi <madawi@atypon.com>
2022-12-26 16:53:14 +0300

    IT-18226

--- api_object.go
+++ api_object.go
@@ -3,6 +3,7 @@ package zabbix
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -12,41 +13,35 @@ type APIObject interface {
 	GetAPIModule() string
 }
 
+type ExtraParamsAPIObject interface {
+	GetExtraParams() Params
+}
+
 func (api *API) CreateAPIObject(object APIObject) (err error) {
 	method := fmt.Sprintf("%s.create", object.GetAPIModule())
+	idsKey, err := getIDsKey(object)
+	if err != nil {
+		return
+	}
 	response, err := api.CallWithError(method, object)
 	if err != nil {
 		return
 	}
 	result := response.Result.(map[string]any)
-	id, err := getIDFromCreateResult(result)
+	ids := result[idsKey].([]any)
+	id := ids[0].(string)
 	object.SetID(id)
-	return
-}
-
-func getIDFromCreateResult(result map[string]any) (id string, err error) {
-	for key, value := range result {
-		if strings.HasSuffix(key, "ids") {
-			ids, ok := value.([]any)
-			if !ok || len(ids) == 0 {
-				err = fmt.Errorf("couldn't find id: %s", ids)
-				return
-			}
-			id = ids[0].(string)
-			break
-		}
-	}
 	return
 }
 
 func (api *API) ReadAPIObject(object APIObject) (err error) {
 	var objects []json.RawMessage
 	method := fmt.Sprintf("%s.get", object.GetAPIModule())
-	filter, err := getIDReadFilter(object)
+	params, err := generateReadParams(object)
 	if err != nil {
 		return
 	}
-	err = api.CallWithErrorParse(method, Params{"filter": filter}, &objects)
+	err = api.CallWithErrorParse(method, params, &objects)
 	if err != nil {
 		return
 	}
@@ -61,30 +56,15 @@ func (api *API) ReadAPIObject(object APIObject) (err error) {
 	return
 }
 
-// Returns a filter as a map of the form:
-//
-//	{
-//			"objectids": object.GetID()
-//	}
-//
-// Used because every zabbix api object has different
-// key for the id
-func getIDReadFilter(object APIObject) (filter map[string]any, err error) {
-	filter = make(map[string]any)
-	var jsonObject map[string]any
-	data, err := json.Marshal(object)
-	if err != nil {
-		return
+func generateReadParams(object APIObject) (params Params, err error) {
+	_, ok := object.(ExtraParamsAPIObject)
+	if ok {
+		params = object.(ExtraParamsAPIObject).GetExtraParams()
+	} else {
+		params = Params{}
 	}
-	err = json.Unmarshal(data, &jsonObject)
-	if err != nil {
-		return
-	}
-	for key, value := range jsonObject {
-		if value == object.GetID() {
-			filter[key] = value
-		}
-	}
+	idsKey, err := getIDsKey(object)
+	params[idsKey] = object.GetID()
 	return
 }
 
@@ -97,5 +77,20 @@ func (api *API) UpdateAPIObject(object APIObject) (err error) {
 func (api *API) DeleteAPIObject(object APIObject) (err error) {
 	method := fmt.Sprintf("%s.delete", object.GetAPIModule())
 	_, err = api.CallWithError(method, []string{object.GetID()})
+	return
+}
+
+func getIDsKey(object APIObject) (idsKey string, err error) {
+	reflectType := reflect.ValueOf(object).Elem().Type()
+	for i := 0; i < reflectType.NumField(); i++ {
+		field := reflectType.Field(i)
+		if field.Tag.Get("zabbix") == "id" {
+			jsonTag := field.Tag.Get("json")
+			jsonKey := strings.Split(jsonTag, ",")[0]
+			idsKey = fmt.Sprintf("%ss", jsonKey)
+			return
+		}
+	}
+	err = fmt.Errorf("%s type doesn't have a field with the tag zabbix:\"id\"", reflectType)
 	return
 }
