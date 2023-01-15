4716efb6f714137380fc9bd6ff0d60eb45cd50ed
Malek Adawi <madawi@atypon.com>
2023-01-15 12:41:04 +0300

    IT-18302

--- auth.go
+++ auth.go
@@ -1,31 +1,31 @@
 package zabbix
 
 type AuthenticationSettings struct {
-	AuthenticationType      int    `json:"authentication_type,string"`
-	HttpAuthEnabled         int    `json:"http_auth_enabled,string"`
-	HttpLoginForm           int    `json:"http_login_form,string"`
-	HttpStripDomains        string `json:"http_strip_domains"`
-	HttpCaseSensitive       int    `json:"http_case_sensitive,string"`
-	LdapConfigured          int    `json:"ldap_configured,string"`
-	LdapCaseSensitive       int    `json:"ldap_case_sensitive,string"`
-	LdapUserdirectoryid     int    `json:"ldap_userdirectoryid,string,omitempty"`
-	SamlAuthEnabled         int    `json:"saml_auth_enabled,string"`
-	SamlIdpEntityid         string `json:"saml_idp_entityid"`
-	SamlSsoUrl              string `json:"saml_sso_url"`
-	SamlSloUrl              string `json:"saml_slo_url"`
-	SamlUsernameAttribute   string `json:"saml_username_attribute"`
-	SamlSpEntityid          string `json:"saml_sp_entityid"`
-	SamlNameidFormat        string `json:"saml_nameid_format"`
-	SamlSignMessages        int    `json:"saml_sign_messages,string"`
-	SamlSignAssertions      int    `json:"saml_sign_assertions,string"`
-	SamlSignAuthnRequests   int    `json:"saml_sign_authn_requests,string"`
-	SamlSignLogoutRequests  int    `json:"saml_sign_logout_requests,string"`
-	SamlSignLogoutResponses int    `json:"saml_sign_logout_responses,string"`
-	SamlEncryptNameid       int    `json:"saml_encrypt_nameid,string"`
-	SamlEncryptAssertions   int    `json:"saml_encrypt_assertions,string"`
-	SamlCaseSensitive       int    `json:"saml_case_sensitive,string"`
-	PasswdMinLength         int    `json:"passwd_min_length,string,omitempty"`
-	PasswdCheckRules        int    `json:"passwd_check_rules,string"`
+	AuthenticationType int    `json:"authentication_type,string,omitempty"`
+	HttpAuthEnabled    int    `json:"http_auth_enabled,string,omitempty"`
+	HttpLoginForm      int    `json:"http_login_form,string,omitempty"`
+	HttpStripDomains   string `json:"http_strip_domains,omitempty"`
+	HttpCaseSensitive  int    `json:"http_case_sensitive,string,omitempty"`
+	LdapConfigured     int    `json:"ldap_configured,string,omitempty"`
+	LdapCaseSensitive  int    `json:"ldap_case_sensitive,string,omitempty"`
+	//LdapUserdirectoryid     string `json:"ldap_userdirectoryid,string,omitempty"`
+	SamlAuthEnabled         int    `json:"saml_auth_enabled,string,omitempty"`
+	SamlIdpEntityid         string `json:"saml_idp_entityid,omitempty"`
+	SamlSsoUrl              string `json:"saml_sso_url,omitempty"`
+	SamlSloUrl              string `json:"saml_slo_url,omitempty"`
+	SamlUsernameAttribute   string `json:"saml_username_attribute,omitempty"`
+	SamlSpEntityid          string `json:"saml_sp_entityid,omitempty"`
+	SamlNameidFormat        string `json:"saml_nameid_format,omitempty"`
+	SamlSignMessages        int    `json:"saml_sign_messages,string,omitempty"`
+	SamlSignAssertions      int    `json:"saml_sign_assertions,string,omitempty"`
+	SamlSignAuthnRequests   int    `json:"saml_sign_authn_requests,string,omitempty"`
+	SamlSignLogoutRequests  int    `json:"saml_sign_logout_requests,string,omitempty"`
+	SamlSignLogoutResponses int    `json:"saml_sign_logout_responses,string,omitempty"`
+	SamlEncryptNameid       int    `json:"saml_encrypt_nameid,string,omitempty"`
+	SamlEncryptAssertions   int    `json:"saml_encrypt_assertions,string,omitempty"`
+	SamlCaseSensitive       int    `json:"saml_case_sensitive,string,omitempty"`
+	//PasswdMinLength         int    `json:"passwd_min_length,string,omitempty"`
+	//PasswdCheckRules int `json:"passwd_check_rules,string,omitempty"`
 }
 
 func (api *API) AuthGet() (authenticationSettings *AuthenticationSettings, err error) {
