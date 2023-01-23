cc35af7889fc868fbad6b4199d9110a5d0ec5e8d
Malek Adawi <madawi@atypon.com>
2023-01-23 14:21:34 +0300

    IT-18271

--- proxy_test.go
+++ proxy_test.go
@@ -1,18 +1,41 @@
 package zabbix_test
 
 import (
+	"encoding/json"
 	zapi "github.com/claranet/go-zabbix-api"
+	"log"
 	"testing"
 )
 
-func TestProxy(t *testing.T) {
-	//proxyInterfaces := &zapi.ProxyInterface{IP: "10.1.1.1", Port: "1234", UseIP: 1}
-	proxy := &zapi.Proxy{Name: "TestZabbixProxy", Status: zapi.ActiveProxy} //, Interface: proxyInterfaces}
-	//testCRUDAPIObjectOperations(t, proxy)
-	testCreateAPIObject(t, proxy)
-	defer testDeleteAPIObject(t, proxy)
-	proxy = &zapi.Proxy{ProxyID: proxy.ProxyID}
-	testReadAPIObject(t, proxy)
-	testUpdateAPIObject(t, proxy)
+func TestPassiveProxy(t *testing.T) {
+	proxyInterfaces := &zapi.ProxyInterface{IP: "10.1.1.1", Port: "1234", UseIP: 1}
+	passiveProxy := &zapi.Proxy{Name: "TestZabbixProxy", Status: zapi.PassiveProxy, Interface: proxyInterfaces}
+	log.Printf("%+v", passiveProxy)
+	//testCRUDAPIObjectOperations(t, passiveProxy)
+	testCreateAPIObject(t, passiveProxy)
+	defer testDeleteAPIObject(t, passiveProxy)
+	passiveProxy = &zapi.Proxy{ProxyID: passiveProxy.ProxyID}
+	testReadAPIObject(t, passiveProxy)
+	jsonInterface, err := json.Marshal(passiveProxy.Interface)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = json.Unmarshal(jsonInterface, proxyInterfaces)
+	if err != nil {
+		t.Fatal(err)
+	}
+	passiveProxy.Interface = proxyInterfaces
+	log.Printf("%+v", passiveProxy)
+	testUpdateAPIObject(t, passiveProxy)
+}
 
+func TestActiveProxy(t *testing.T) {
+	activeProxy := &zapi.Proxy{Name: "TestZabbixProxy", Status: zapi.ActiveProxy, Interface: nil}
+	log.Printf("%+v", activeProxy)
+	//testCRUDAPIObjectOperations(t, activeProxy)
+	testCreateAPIObject(t, activeProxy)
+	defer testDeleteAPIObject(t, activeProxy)
+	activeProxy = &zapi.Proxy{ProxyID: activeProxy.ProxyID}
+	testReadAPIObject(t, activeProxy)
+	testUpdateAPIObject(t, activeProxy)
 }
